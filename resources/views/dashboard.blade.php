5acf28b4de3f6387d9a082aa5ea3e7df7932ede9
wirdaalmahera <Wirdaalmahera@smkwikrama.sch.id>
2022-11-09 14:31:04 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('main')
+
+@section('content')
+
+<div class="card">
+	<div class="card-header">Dashboard</div>
+	<div class="card-body">
+		
+		Selamat, kamu berhasil login 
+	</div>
+</div>
+
+@endsection('content')
